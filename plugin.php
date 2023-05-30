c472b1e078151b83db02375fa87e491ee759d8a7
Martin Studer <martin@fluxlabs.ch>
2023-05-30 05:22:18 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 // alphanumerical ID of the plugin; never change this
 $id = 'dhbwparticipationpdf';
-$version = '1.0.3';
+$version = '1.1.0';
 $ilias_min_version = "6.0";
 $ilias_max_version = "7.999";
 $responsible = "fluxlabs ag";
