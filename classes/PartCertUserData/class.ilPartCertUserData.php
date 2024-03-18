876fb5b20cf34e6de02707dcddd66775b9575cfc
ingoj <120396930+ingoj@users.noreply.github.com>
2024-03-18 18:01:14 +0100

    Fix nullable type hints

--- classes/PartCertUserData/class.ilPartCertUserData.php
+++ classes/PartCertUserData/class.ilPartCertUserData.php
@@ -1,10 +1,10 @@
 <?php
 class ilPartCertUserData {
 	protected int $part_cert_usr_id;
-	protected string $part_cert_firstname;
-	protected string $part_cert_lastname;
-	protected string $part_cert_gender;
-	protected string $part_cert_salutation;
+	protected ?string $part_cert_firstname;
+	protected ?string $part_cert_lastname;
+	protected ?string $part_cert_gender;
+	protected ?string $part_cert_salutation;
 	protected string $part_cert_username;
 
 	public function getPartCertUsrId(): int
@@ -15,35 +15,35 @@ class ilPartCertUserData {
     {
 		$this->part_cert_usr_id = $part_cert_usr_id;
 	}
-	public function getPartCertFirstname(): string
+	public function getPartCertFirstname(): ?string
     {
 		return $this->part_cert_firstname;
 	}
-	public function setPartCertFirstname(string $part_cert_firstname): void
+	public function setPartCertFirstname(?string $part_cert_firstname): void
     {
 		$this->part_cert_firstname = $part_cert_firstname;
 	}
-	public function getPartCertLastname(): string
+	public function getPartCertLastname(): ?string
     {
 		return $this->part_cert_lastname;
 	}
-	public function setPartCertLastname(string $part_cert_lastname): void
+	public function setPartCertLastname(?string $part_cert_lastname): void
     {
 		$this->part_cert_lastname = $part_cert_lastname;
 	}
-	public function getPartCertGender(): string
+	public function getPartCertGender(): ?string
     {
 		return $this->part_cert_gender;
 	}
-	public function setPartCertGender(string $part_cert_gender): void
+	public function setPartCertGender(?string $part_cert_gender): void
     {
 		$this->part_cert_gender = $part_cert_gender;
 	}
-	public function getPartCertSalutation(): string
+	public function getPartCertSalutation(): ?string
     {
 		return $this->part_cert_salutation;
 	}
-	public function setPartCertSalutation(string $part_cert_salutation): void
+	public function setPartCertSalutation(?string $part_cert_salutation): void
     {
 		$this->part_cert_salutation = $part_cert_salutation;
 	}
