bdd59f3cd876fd5f49b3dc13ec2f6454d00d2a04
Ioanna Mitroulaki <ioanna.mitroulaki@kroepelin-projekte.de>
2025-03-19 10:01:45 +0000

    Remove require_once, change min, max version

--- classes/class.ilParticipationCertificateConfigGUI.php
+++ classes/class.ilParticipationCertificateConfigGUI.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once __DIR__ . "/../vendor/autoload.php";
-
 //TODO Refactoring - find a better way to save and display the form
 
 /**
--- plugin.php
+++ plugin.php
@@ -1,9 +1,9 @@
 <?php
 // alphanumerical ID of the plugin; never change this
 $id = 'dhbwparticipationpdf';
-$version = '1.1.0';
-$ilias_min_version = "8.0";
-$ilias_max_version = "8.999";
-$responsible = "fluxlabs ag";
-$responsible_mail = "support@fluxlabs.ch";
+$version = '1.1.0'; // TODO ???
+$ilias_min_version = "9.0";
+$ilias_max_version = "9.999";
+$responsible = "fluxlabs ag"; // TODO ???
+$responsible_mail = "support@fluxlabs.ch"; // TODO ???
 ?>
