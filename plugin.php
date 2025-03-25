7a47fb881584c8e420df68379f55a448050adaf0
Ioanna Mitroulaki <ioanna.mitroulaki@kroepelin-projekte.de>
2025-03-25 14:01:06 +0100

    Update plugins data

--- plugin.php
+++ plugin.php
@@ -1,9 +1,9 @@
 <?php
 // alphanumerical ID of the plugin; never change this
 $id = 'dhbwparticipationpdf';
-$version = '1.2.0'; // TODO ???
+$version = '1.2.0';
 $ilias_min_version = "9.0";
 $ilias_max_version = "9.999";
-$responsible = "fluxlabs ag"; // TODO ???
-$responsible_mail = "support@fluxlabs.ch"; // TODO ???
+$responsible = "Kröpelin Projekt GmbH ";
+$responsible_mail = "info@kroepelin-projekte.de";
 ?>
