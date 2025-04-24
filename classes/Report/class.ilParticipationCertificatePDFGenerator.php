6034f1729d9e642c3c853767fdc1a699a9018e15
Ioanna Mitroulaki <ioanna.mitroulaki@kroepelin-projekte.de>
2025-04-24 13:16:24 +0200

    Remove code for testing

--- classes/Report/class.ilParticipationCertificatePDFGenerator.php
+++ classes/Report/class.ilParticipationCertificatePDFGenerator.php
@@ -58,9 +58,6 @@ class ilParticipationCertificatePDFGenerator
         if ($total_users == 1) {
             $mpdf->WriteHTML($css, 1);
 
-            // TODO Remove it
-            $rendered = str_replace('http://localhost:8413', 'http://host.docker.internal:8413', $rendered);
-
             $mpdf->WriteHTML($rendered, 2);
             $mpdf->Output($this->pl->txt("plugin") . '.pdf', 'D');
             if (method_exists($this->tpl, 'loadStandardTemplate')) {
@@ -78,9 +75,6 @@ class ilParticipationCertificatePDFGenerator
         } /*Checkt ob es der letzte Durchlauf ist. Wenn ja wird das letzte PDF erzeugt und das vorhandene PDF auf dem Server
 			 *wird hinten an das erzeugte PDF angehängt. Anschliessend wird das fertige PDF dem User im Browser als Download angeboten.
 			*/ elseif ($printCount == $total_users) {
-
-            // TODO Remove it
-            $rendered = str_replace('http://localhost:8413', 'http://host.docker.internal:8413', $rendered);
 
             $mpdf->WriteHTML($css, 1);
             $mpdf->WriteHTML($rendered, 2);
