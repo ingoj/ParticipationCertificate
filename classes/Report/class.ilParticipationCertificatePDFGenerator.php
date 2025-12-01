623b6616e0bac57cec9c7e604f822236916cb957
Ioanna Mitroulaki <ioanna.mitroulaki@kroepelin-projekte.de>
2025-12-01 16:56:23 +0100

    Remove code for dev env

--- classes/Report/class.ilParticipationCertificatePDFGenerator.php
+++ classes/Report/class.ilParticipationCertificatePDFGenerator.php
@@ -66,9 +66,8 @@ class ilParticipationCertificatePDFGenerator
             'tempDir' => '/tmp/mpdf',
             'default_font' => 'dejavusans'
         ]);
-        $rendered = str_replace('http://localhost:8428', 'http://host.docker.internal:8428', $rendered);
-        //Css file wird geladen
 
+        //Css file wird geladen
         $css = file_get_contents('./' . ilParticipationCertificatePlugin::PLUGIN_DIRECTORY . '/templates/report/Teilnahmebescheinigung.css');
         $printCount++;
 
